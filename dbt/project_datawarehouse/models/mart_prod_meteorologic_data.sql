1f68e4c4df28ed95ce1390e939d6b0eec53eb90f
Gabriel-elk <gtcarvalho2005@gmail.com>
2026-08-04 17:04:30 -0300

    union all tables creation

--- dbt/project_datawarehouse/models/mart_prod_meteorologic_data.sql
+++ dbt/project_datawarehouse/models/mart_prod_meteorologic_data.sql
@@ -1,4 +1,17 @@
-SELECT d.data_hora, d.temperature, d.felt_temperature, d.wind_speed, d.precipitation_probability,
-o.apparent_temperature, o.precipitation, o.cloud_cover, o.visibility, o.wind_speed_10m, o.soil_temperature_0cm
-from staging.stg_tests_meteo_blue_data as d
-join staging.stg_tests_open_meteo_data as o on o.data_hora = d.data_hora
+
+
+-- {{config(materialized='incremental',
+-- unique_key='id')}}
+-- with mart_meteorologic_data as (
+-- SELECT d.id, d.data_hora, d.temperature, d.felt_temperature, d.wind_speed, d.precipitation_probability
+--     FROM {{ source('stg', 'stg_tests_meteo_blue_data') }}
+--     UNION ALL
+-- SELECT 
+-- o.id, o.data_hora, o.apparent_temperature, o.precipitation, o.cloud_cover, o.visibility, o.wind_speed_10m, o.soil_temperature_0cm
+--     FROM {{ source('stg', 'stg_tests_open_meteo_data') }}
+
+--     {%if is_incremental() %}
+--     where raw.date_time > (select coalesce(max(data_hora))from {{this}})
+--     {% endif %}
+-- )
+-- select * from mart_meteorologic_data
